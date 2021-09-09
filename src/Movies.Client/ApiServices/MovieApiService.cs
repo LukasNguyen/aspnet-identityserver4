b4806dc265216d379fa4fd3e63777ceba69e7388
Lukas Nguyen <lukas.nguyen@contemi.com>
2021-09-09 15:59:17 +0700

    Added Movies.Client microservice.

---
+++ src/Movies.Client/ApiServices/MovieApiService.cs
@@ -0,0 +1,58 @@
+﻿using Movies.Client.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies.Client.ApiServices
+{
+    public class MovieApiService : IMovieApiService
+    {
+        public Task<IEnumerable<Movie>> GetMovies()
+        {
+            return Task.FromResult(new List<Movie>
+                {
+                    new Movie
+                    {
+                        Id = 1,
+                        Genre = "Drama",
+                        Title = "The Shawshank Redemption",
+                        Rating = "9.3",
+                        ImageUrl = "images/src",
+                        ReleaseDate = new DateTime(1994, 5, 5),
+                        Owner = "alice"
+                    },
+                    new Movie
+                    {
+                        Id = 2,
+                        Genre = "Crime",
+                        Title = "The Godfather",
+                        Rating = "9.2",
+                        ImageUrl = "images/src",
+                        ReleaseDate = new DateTime(1972, 5, 5),
+                        Owner = "alice"
+                    } }.AsEnumerable());
+        }
+
+        public Task<Movie> CreateMovie(Movie movie)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteMovie(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Movie> GetMovie(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Movie> UpdateMovie(Movie movie)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
